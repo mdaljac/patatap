8e9a60839cb975a91c322f279634dbeb04aa8fc7
mdaljac <mdaljac@gmail.com>
2017-05-07 23:31:22 +0200

    Optimizing app
    Removing elements from an array that are not visible anymore.

--- myScript.js
+++ myScript.js
@@ -3,32 +3,35 @@
 		var sounds = ['bubbles', 'clay', 'corona', 'moon', 'splits', 'strike', 'ufo', 'glimmer', 'timer', 'wipe', 'veil', 'piston-1',
 						'piston-2', 'piston-3', 'flash-1', 'flash-2', 'flash-3'];
 
-
 		function onKeyDown(event) {
 
 			var maxPoint = new Point(window.innerWidth, window.innerHeight); //maximum possible point in view
 			var randomPoint = Point.random(); //x random, y random
 			var myPoint = maxPoint * randomPoint
 			var randomSong = Math.floor(Math.random() * sounds.length);
+			var randomBackground = Math.floor(Math.random()*255) + "," + Math.floor(Math.random()*255) + "," + Math.floor(Math.random()*255);
 
 			myCircle = new Path.Circle(myPoint, 300);
-			myCircle.fillColor = 'blue';
+			myCircle.fillColor = "rgb(" + randomBackground + ")";
 			circles.push(myCircle);
 
 			var sound = new Howl({
 				src: ["sounds/" + sounds[randomSong] + ".mp3"]
 			});
-
 			sound.play();
-
-
-
 		}
 
 		function onFrame(event) {
 
 			for (var i=0; i<circles.length; i++){
-				circles[i].fillColor.hue += 1;
-				circles[i].scale(0.9);
+
+				if (circles[i].area < 1) {
+					circles[i].remove();
+					circles.splice(i, 1);
+				}
+				else{
+					circles[i].fillColor.hue += 1;
+					circles[i].scale(0.9);
+				}	
 			}
 		}
